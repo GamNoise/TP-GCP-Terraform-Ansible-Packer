52ae150a35a3706a8b0fda487caeab0e1b75917e
GamNoise <hallerayvalentin@aol.fr>
2025-01-10 16:02:57 +0100

    [EDIT] Modification des repertoires et variables du main.tf OPS

--- environments/ops/main.tf
+++ environments/ops/main.tf
@@ -11,54 +11,27 @@ provider "google" {
 }
 
 module "artifact_registry" {
-  source = "./modules/artifact_registry"
+  source = "../../modules/artifact_registry"
 
-  name        = var.registry_name
-  location    = var.region
-  description = "Artifact registry for Go binaries"
+  repository_name = var.artifact_registry_name
+  location        = var.region
 
-  labels = {
-    environment = var.environment
-    project     = var.project
-  }
 }
 
 module "storage" {
-  source = "./modules/storage"
-
-  bucket_name = var.terraform_state_bucket
+  source = "../../modules/storage"
+  bucket_name = "terraform-state-${var.project_id}"
   location    = var.region
-
-  versioning = {
-    enabled = true
-  }
-
-  iam_configuration = {
-    uniform_bucket_level_access = true
-  }
-
-  labels = {
-    environment = var.environment
-    project     = var.project
-  }
+  is_public   = false
+  project_id  = var.project_id
+  region      = var.region
 }
 
 module "monitoring" {
-  source = "./modules/monitoring"
-
-  metrics = [
-    "traffic_load_balancer",
-    "cpu_usage",
-    "memory_usage",
-    "http_5xx_errors",
-    "latency_load_balancer"
-  ]
-
-  labels = {
-    environment = var.environment
-    project     = var.project
-  }
+  source    = "../../modules/monitoring"
+  project_id = var.project_id
 }
+
 
 resource "google_storage_bucket" "terraform_state" {
   name                        = "terraform-state-${var.project_id}"
